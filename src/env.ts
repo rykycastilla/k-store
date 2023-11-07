b2a938f8f1beff0adf7af0e87c3fa3dd5092614e
RykyCE <rcastilla744@gmail.com>
2023-11-06 23:04:03 -0500

    Using .env files

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,9 @@
-export const BACKEND_URL = 'http://localhost:8000/'
+import productionEnv from './env.production.json'
+
+interface Env { EXPO_PUBLIC_BACKEND_URL:string }
+
+const env: Env = process.env.EXPO_PUBLIC_BACKEND_URL
+  ? process.env as unknown as Env
+  : productionEnv
+
+export const { EXPO_PUBLIC_BACKEND_URL:BACKEND_URL } = env
