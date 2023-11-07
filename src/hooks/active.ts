e8074ddedc478e7f1e3c24d6d96596edb069b878
RykyCE <rcastilla744@gmail.com>
2023-11-06 21:12:49 -0500

    Fixing useActive bug

--- src/hooks/active.ts
+++ src/hooks/active.ts
@@ -1,13 +1,17 @@
 import { AppState } from 'react-native'
 import { FunctionVoid } from '../types'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 
 function useActive( onactived:FunctionVoid ) {
-  useEffect( () => {
-    const activeHandler: FunctionVoid = () => {
+  const activeHandler: FunctionVoid = useMemo( () => {
+    return () => {
       const { currentState } = AppState
       if( currentState === 'active' ) { onactived() }
     }
+  }, [ onactived ] )
+  useEffect( () => activeHandler(), [] )  // Executing for the first time
+  // Updating callback
+  useEffect( () => {
     const listener = AppState.addEventListener( 'change', activeHandler )
     return listener.remove
   }, [ onactived ] )
